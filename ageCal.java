84254351df160f29d614e92155fa9690b1272c83
DESKTOP-VUEA0AQ\kopo24 <lss2star@gmail.com>
2023-04-26 16:34:28 +0900

    나이 계산 하는 함수 구체화1

--- ageCal.java
+++ ageCal.java
@@ -1,24 +1,75 @@
 package LandTicket;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ageCal {
 	public int getAge() {
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Integer> seperateId = new ArrayList<>();
+		LocalDate now = LocalDate.now();
 		while (true) {
-		System.out.println("13자리의 숫자를 입력 : ");
-		String id = sc.nextLine();
-		
-		if (id.length() != 13) {
-			System.out.println("다시 입력하세요 : ");
-			id = sc.nextLine();
-			continue;
+			seperateId.clear();
+			System.out.println("13자리의 숫자를 입력 : ");
+			String id = sc.nextLine();
+
+			if (id.length() != 13) {
+				System.out.println("다시 입력하세요 : ");
+				id = sc.nextLine();
+				continue;
+			}
+
+			for (int i = 0; i < id.length(); i++) {
+				seperateId.add(Character.getNumericValue(id.charAt(i)));
+			}
+			int birthYear = 10 * seperateId.get(0) + seperateId.get(1);
+			int birthMonth = 10 * seperateId.get(2) + seperateId.get(3);
+			int birthDay = 10 * seperateId.get(4) + seperateId.get(5);
+			int flag = seperateId.get(6);
+
+			switch (flag) {
+			case 1:
+			case 2:
+				birthYear =  birthYear + 1900;
+				break;
+
+			default:
+				birthYear =  birthYear + 2000;
+				break;
+			}
+			if ((birthYear > 12) || (birthYear) <=0 || (birthDay > 31)) {
+				System.out.println("다시 입력해주세요");
+				continue;
+			}
+			
+			switch (flag) {
+			case 1:
+			case 2:
+				if (birthYear >= 1900 && birthYear <= 1999) {
+					System.out.println("정확한 입력입니다");
+					break;
+				}
+				else if (birthYear >= 2000) {
+					System.out.println("잘못된 입력입니다. 다시입력해주세요");
+					continue;
+				}
+			case 3:
+			case 4:
+				if (birthYear >= 2000 && birthYear <= 2999) {
+					System.out.println("정확한 입력입니다");
+					break;
+				}
+				else if (birthYear < 2000) {
+					System.out.println("잘못된 입력입니다. 다시입력해주세요");
+					continue;
+				}
+			}
+			
+			int age = now.getYear() - birthYear;
+			if (birthMonth > now.getMonthValue() || birthMonth == now.getMonthValue() && birthMonth > now.getMonthValue()) age--;
+			
+			return age;
 		}
-		
-		for (int i = 0; i < id.length(); i++) {
-			seperateId.add(Character.getNumericValue(id.charAt(i)));
-		}
-		
-		
-		
-		}
+	}
 }
