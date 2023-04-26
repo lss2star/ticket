21b3b3d863c3d9ba64f7baa676f8636ea625b05b
DESKTOP-VUEA0AQ\kopo24 <lss2star@gmail.com>
2023-04-26 16:34:51 +0900

    출력값 input에서 받아서 수정하기 1 + 구체화 필요

--- output.java
+++ output.java
@@ -1,12 +1,14 @@
 package LandTicket;
 
-public class output {
+public class Output {
+	static Input input = new Input();
 	public void asktype() {
 		System.out.println("권종을 선택하세요");
 		System.out.println("1. 주간권");
 		System.out.println("2. 야간권");
+		input.ticketType();
 	}
-	public void askage() {
+	public static void askage() {
 		System.out.println("13자리 숫자를 입력하세요 :");
 	}
 	public void askPrefer() {
@@ -16,6 +18,21 @@ public class output {
 		System.out.println("3. 국가유공자");
 		System.out.println("4. 다자녀");
 		System.out.println("5. 임산부");
+		input.selectPersonType();
+	}
+	public void askEa() {
+		System.out.println("몇 개를 주문하시겠습니까? (최대 10개)");
+		input.amountEa();
+	}
+	public void totalPrice() {
+		System.out.println("총 %d원 입니다");      //  total가격 확인해서 넣어줘야함
+		System.out.println("감사합니다");
+	}
+	public void continueOreder () {
+		System.out.println("계속 발권하시겠습니다?");
+		System.out.println("1. 티켓 발권");
+		System.out.println("2. 종료");
+		input.order();
 	}
 
 }
