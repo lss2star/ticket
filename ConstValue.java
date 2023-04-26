28f75dd05e179e559f7416069010950280b77fae
DESKTOP-VUEA0AQ\kopo24 <lss2star@gmail.com>
2023-04-26 13:37:58 +0900

    모든 변수 클래스로 설정

---
+++ ConstValue.java
@@ -0,0 +1,25 @@
+package LandTicket;
+
+public class ConstValue {
+	 // 고정으로 사용할 변수들
+        //주간 야간 나이별 금액
+        public static final	int adult_dayTime_price = 56000;
+        public static final int juv_dayTime_price = 47000;
+        public static final int child_dayTime_price = 44000;
+        public static final int senior_dayTime_price = 44000;
+        public static final int infant_dayTime_price = 0;
+
+        public static final int adult_nightTime_price = 46000;
+        public static final int juv_nightTime_price = 40000;
+        public static final int child_nightTime_price = 37000;
+        public static final int senior_nightTime_price = 37000;
+        public static final int infant_nightTime_price = 0;
+        
+        //할인율
+        public static final double able = 0.0;
+        public static final double disable = 0.4;
+        public static final double merit = 0.5;
+        public static final double multiChild = 0.2;
+        public static final double pregnant = 0.15;
+    
+}
