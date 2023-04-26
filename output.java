1fe710c45b4e9f3641a099953893b4e29d25a684
DESKTOP-VUEA0AQ\kopo24 <lss2star@gmail.com>
2023-04-26 13:43:20 +0900

    문구 출력 클래스

---
+++ output.java
@@ -0,0 +1,21 @@
+package LandTicket;
+
+public class output {
+	public void asktype() {
+		System.out.println("권종을 선택하세요");
+		System.out.println("1. 주간권");
+		System.out.println("2. 야간권");
+	}
+	public void askage() {
+		System.out.println("13자리 숫자를 입력하세요 :");
+	}
+	public void askPrefer() {
+		System.out.println("우대사항을 선택하세요.");
+		System.out.println("1. 없음");
+		System.out.println("2. 장애인");
+		System.out.println("3. 국가유공자");
+		System.out.println("4. 다자녀");
+		System.out.println("5. 임산부");
+	}
+
+}
